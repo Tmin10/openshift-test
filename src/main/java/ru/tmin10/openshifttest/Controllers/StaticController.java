ae71e205f660dd519c2874c3128cc79eaafce911
Tmin10 <tmin100@gmail.com>
2018-08-21 23:43:57 +0300

    Add static page

---
+++ src/main/java/ru/tmin10/openshifttest/Controllers/StaticController.java
@@ -0,0 +1,13 @@
+package ru.tmin10.openshifttest.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StaticController {
+    @RequestMapping(value={"/test", "/exit", "/error-page"})
+    public String getStatic()
+    {
+        return "index.html";
+    }
+}
